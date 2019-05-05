fe8b33a8184439ca8202122e9e2606d3e2e187c1
Vicente Moraes <vicenteromeiromoraes@gmail.com>
2019-05-05 17:29:24 -0300

    Fixed bugs

--- smtp.py
+++ smtp.py
@@ -5,11 +5,11 @@ import ssl
 class SMTP:
     def __init__(self, mailServer, mailPort, login, password, subject, receiveAdress, message):
         self.receiveAdress = receiveAdress
-        self.endmsg = "\r\n \r\n"
+        self.endmsg = "\r\n"
         self.mailServer = mailServer
-        self.clientSocket = socket.socket(AF_INET, SOCK_STREAM)
-        self.sslclientSocket = null
-        self.mailPort = mailPort
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sslclientSocket = None
+        self.mailPort = mailPort #Port 587 for STARTTLS, Port 465 for SSL
         self.login = login
         self.password = password
         self.subject = subject
@@ -19,55 +19,55 @@ class SMTP:
     def _validate_(self, recv, reply):
         print(recv)
         if recv[:3] != reply:
-            print(reply+" reply not received")
+            print(reply.decode() +" reply not received")
             return False
         return True
 
     def _connect_(self, socket):
         socket.connect((self.mailServer, self.mailPort))
         recv = socket.recv(1024)
-        return self._validate_(recv, '220')
+        return self._validate_(recv, b'220')
 
     def _HELO_(self, socket):
         helomsg = "HELO Alice" + self.endmsg
-        socket.send(helomsg)
+        socket.send(helomsg.encode())
         recv = socket.recv(1024)
-        return self._validate_(recv, '250')
+        return self._validate_(recv, b'250')
 
     def _STARTTLS_(self, socket):
         starttls = "STARTTLS" + self.endmsg
-        socket.send(starttls)
+        socket.send(starttls.encode())
         recv = socket.recv(1024)
-        return self._validate_(recv, '220')
+        return self._validate_(recv, b'220')
         
     def _AUTH_(self, socket):
-        authmsg = "\00" + self.login + "\00" + self.password
-        authmsg = "AUTH PLAIN " + base64.b64encode(auth.encode("utf-8")) + self.endmsg
-        socket.send(authmsg)
+        authmsg = base64.b64encode(("\00" + self.login + "\00" + self.password).encode())
+        authmsg = "AUTH PLAIN " + str(authmsg.decode()) + self.endmsg
+        socket.send(authmsg.encode())
         recv = socket.recv(1024)
-        return self._validate_(recv, '235')
+        return self._validate_(recv, b'235')
 
     def _MAIL_(self, socket):
-        mail = "MAIL FROM: <" + login + ">" + self.endmsg
-        socket.send(mail)
+        mail = "MAIL FROM: <" + self.login + ">" + self.endmsg
+        socket.send(mail.encode())
         recv = socket.recv(1024)
-        if not self._validate_(recv, '250'):
+        if not self._validate_(recv, b'250'):
             return False
         rcpt = "rcpt to: <" + self.receiveAdress + ">" + self.endmsg
-        socket.send(rcpt)
+        socket.send(rcpt.encode())
         recv = socket.recv(1024)
-        return self._validate_(recv, '250')
+        return self._validate_(recv, b'250')
 
     def _DATA_(self, socket):
-        socket.send("DATA" + self.endmsg)
+        socket.send("DATA\r\n".encode())
         recv = socket.recv(1024)
-        if not self._validate_(recv, '354')
+        if not self._validate_(recv, b'354'):
             print("Could not send DATA")
             return False
         msg = "Subject: " + self.subject + self.endmsg + self.message + self.endmsg + "." + self.endmsg
-        socket.send(msg)
+        socket.send(msg.encode())
         recv = socket.recv(1024)
-        return self._validate_(recv, '250')
+        return self._validate_(recv, b'250')
 
     def send(self):
         if not self._connect_(self.clientSocket):
@@ -75,17 +75,17 @@ class SMTP:
             return False
         print("Connection established")
         if not self._HELO_(self.clientSocket):
-            print("No response from server")
+            print("No helo")
             return False
-        print("Connection with server established")
+        print("helo received")
         if not self._STARTTLS_(self.clientSocket):
-            print("No response from server")
+            print("No STARTTTLS")
             return False
         self.sslclientSocket = ssl.wrap_socket(self.clientSocket)
         if not self._HELO_(self.sslclientSocket):
-            print("No response from server")
+            print("No helo")
             return False
-        print("Server responded") 
+        print("helo received") 
         if not self._AUTH_(self.sslclientSocket ):
             print("Could not autenticate credentials")
             return False
@@ -96,10 +96,10 @@ class SMTP:
         if not self._DATA_(self.sslclientSocket):
             print("Could not send data")
             return False
-        self.sslclientSocket.send("quit" + self.endmsg)
+        self.sslclientSocket.send("quit\r\n".encode())
         recv = self.sslclientSocket.recv(1024)
-        if self._validate_(recv, '221'):
+        if self._validate_(recv, b'221'):
             print("Email sent!")
             return True
-        print("Could not send email")
+        print("Could not quit")
         return False
